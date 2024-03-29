2635017e2be306fffbc77dff4b178a8194c92c53
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2024-03-29 11:39:06 +0300

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,37 +1,36 @@
 const ApiError = require('../error/ApiError');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const { User, Basket } = require('../models/models')
+const {User, Basket} = require('../models/models')
 
 const generateJwt = (id, email, role) => {
     return jwt.sign(
-        { id, email, role },
+        {id, email, role},
         process.env.SECRET_KEY,
-        { expireIn: '24h' }
+        {expiresIn: '24h'}
     )
-
 }
 
-class userController {
+class UserController {
     async registration(req, res, next) {
-        const { email, password, role } = req.body
+        const {email, password, role} = req.body
         if (!email || !password) {
-            return next(ApiError.badRequest('Некорретный email или password'))
+            return next(ApiError.badRequest('Некорректный email или password'))
         }
-        const candidate = await User.findOne({ where: { email } })
+        const candidate = await User.findOne({where: {email}})
         if (candidate) {
             return next(ApiError.badRequest('Пользователь с таким email уже существует'))
         }
         const hashPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({ email, role, password: hashPassword })
-        const basket = await Basket.create({ userId: userId })
-        const token = generateJwt(user, id, user.email, user.role)
-        return res.json({ token })
-
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
+
     async login(req, res, next) {
-        const { email, password } = req.body
-        const user = await User.findOne({ where: { email } })
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
         if (!user) {
             return next(ApiError.internal('Пользователь не найден'))
         }
@@ -42,9 +41,12 @@ class userController {
         const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
+
     async check(req, res, next) {
         const token = generateJwt(req.user.id, req.user.email, req.user.role)
         return res.json({token})
     }
 }
+
+module.exports = new UserController()
 module.exports = new UserController()
